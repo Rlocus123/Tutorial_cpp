dfa45db7b150587181aad8db457c2bd8a1d84ad2
Rlocus123 <pittets@gmail.com>
2020-11-10 21:42:43 +0100

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -46,6 +46,9 @@ int main()
     cout.precision(3);
     cout << "Circle area at initialization : " << fixed << Circle.get_area() << endl;
     cout << "Circle diameter : " << fixed << Circle.get_diameter() << endl;
+	
+	/* The end */
+	cout << "This is the end " << endl ;
 
     return 0;
 }
